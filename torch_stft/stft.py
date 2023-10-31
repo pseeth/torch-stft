ab95bb070d078626b880f357f3aeace1eff05f51
RiderLai <lyf670354671@gmail.com>
2023-10-31 14:55:05 +0800

    Compatible with librosa 0.10

--- torch_stft/stft.py
+++ torch_stft/stft.py
@@ -43,7 +43,7 @@ class STFT(torch.nn.Module):
         assert(filter_length >= self.win_length)
         # get window and zero center pad it to filter_length
         fft_window = get_window(window, self.win_length, fftbins=True)
-        fft_window = pad_center(fft_window, filter_length)
+        fft_window = pad_center(fft_window, size=filter_length)
         fft_window = torch.from_numpy(fft_window).float()
 
         # window the bases
